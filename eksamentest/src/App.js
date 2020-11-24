7fe95224278ee9ef957475aba6726ff474d22891
Laura Toft <laur507p@stud.kea.dk>
2020-11-24 11:29:50 +0100

    structure

--- eksamentest/src/App.js
+++ eksamentest/src/App.js
@@ -1,18 +1,16 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+
 import "./App.css";
+import Animation from "./components/Animation";
+import Queue from "./components/Queue";
+import Order from "./components/Order";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React Test test tesg
-        </a>
-      </header>
+      <Animation />
+      <Queue />
+      <Order />
     </div>
   );
 }
